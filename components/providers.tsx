50cc019ec335da9a84a9120a0a0300016c23bfd1
khaliff868 <khaliff@email.com>
2026-03-22 19:15:20 -0400

    Update providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,11 +4,20 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from './theme-provider';
+import useInactivityLogout from '@/hooks/useInactivityLogout';
+
+// ✅ Wrap hook INSIDE SessionProvider
+function InactivityGuard({ children }: { children: React.ReactNode }) {
+  useInactivityLogout();
+  return <>{children}</>;
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <InactivityGuard>
+        <ThemeProvider>{children}</ThemeProvider>
+      </InactivityGuard>
     </SessionProvider>
   );
 }
